d59d3586b93df37c09208002e43c40b56b4ecd02
mysteriouseye <v1200347@live.com>
2022-01-21 18:18:44 +0800

    commit

--- review/Design-Model/Creation mode/2.Factory/code/src/App.java
+++ review/Design-Model/Creation mode/2.Factory/code/src/App.java
@@ -1,6 +1,10 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
+
+import AbstractFactoryMode.MagicFactory;
+import AbstractFactoryMode.NormalFactory;
 import FactoryMode.AbstractFactory;
 import FactoryMode.EmptyFactory;
 import FactoryMode.ParkFactory;
@@ -58,9 +62,39 @@ public class App {
             }
         }
     }
+
+    public static void AbstractFactoryMode(AbstractFactoryMode.AbstractFactory af) {
+
+       
+        ArrayList<AbstractBlock> map = new ArrayList<>();
+        map.add(af.getEmpty());
+        map.add(af.getPrison());
+        map.add(af.getPark());
+        Random rd = new Random(3);
+        int col=12;
+        for(int i=1;i<=col;i++){
+            int tt = rd.nextInt(3);
+            map.get(tt).printBlock();
+            if(i==4||i==6||i==8)
+                System.out.println();
+            if(i==5||i==7)
+                System.out.print("    ");
+        }
+    }
     public static void main(String[] args) throws Exception {
         SimpleFactoryTest();
         System.out.println("");
         FactoryModeTest();
+        System.out.println("");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("请输入指令（1输出普通地图，2输出魔法地图）");
+        int order;
+        order  = scan.nextInt();
+        if(order == 1){
+            AbstractFactoryMode(new NormalFactory());
+        }else{
+            AbstractFactoryMode(new MagicFactory());
+        }
+        scan.close();
     }  
 }
