90f8fc1f38bed95678edf78b99bd27b8b69774f6
mysteriouseye <v1200347@live.com>
2022-01-24 18:16:49 +0800

    update

--- review/Design-Model/Creation mode/2.Factory/code/src/App.java
+++ review/Design-Model/Creation mode/2.Factory/code/src/App.java
@@ -13,7 +13,7 @@ import SimpleFactory.AbstractBlock;
 import SimpleFactory.SimpleFactoryClass;
 
 public class App {
-    public static void SimpleFactoryTest(){
+    public static void SimpleFactoryTest() {
         SimpleFactoryClass simpleFactoryClass = new SimpleFactoryClass();
         List<AbstractBlock> abstractBlocks = new ArrayList<>();
 
@@ -28,36 +28,37 @@ public class App {
         }
         for (int i = 0; i < 12; i++) {
             abstractBlocks.get(i).printBlock();
-            if(i == 3 || i == 5 || i == 7){
+            if (i == 3 || i == 5 || i == 7) {
                 System.out.println();
             }
-            if(i == 4 || i == 6){
+            if (i == 4 || i == 6) {
                 System.out.print("    ");
             }
         }
     }
+
     // 工厂方法进一步解耦合，把工厂类抽象，不再负责所有实例的创建，而是把具体的创建工作交给了子类完成，实例化延迟到子类加载，由子类来决定要实例化的类。
-    public static void FactoryModeTest(){
+    public static void FactoryModeTest() {
         List<AbstractBlock> abstractBlocks = new ArrayList<>();
         AbstractFactory abstractFactory;
         Random rd = new Random();
         for (int i = 1; i <= 12; i++) {
             int flag = rd.nextInt(3);
-            if(flag == 0){
+            if (flag == 0) {
                 abstractFactory = new EmptyFactory();
-            }else if(flag == 1){
+            } else if (flag == 1) {
                 abstractFactory = new ParkFactory();
-            }else{
+            } else {
                 abstractFactory = new PrisonFactory();
             }
             abstractBlocks.add(abstractFactory.createBlock());
         }
         for (int i = 0; i < 12; i++) {
             abstractBlocks.get(i).printBlock();
-            if(i == 3 || i == 5 || i == 7){
+            if (i == 3 || i == 5 || i == 7) {
                 System.out.println();
             }
-            if(i == 4 || i == 6){
+            if (i == 4 || i == 6) {
                 System.out.print("    ");
             }
         }
@@ -65,22 +66,22 @@ public class App {
 
     public static void AbstractFactoryMode(AbstractFactoryMode.AbstractFactory af) {
 
-       
         ArrayList<AbstractBlock> map = new ArrayList<>();
         map.add(af.getEmpty());
         map.add(af.getPrison());
         map.add(af.getPark());
         Random rd = new Random(3);
-        int col=12;
-        for(int i=1;i<=col;i++){
+        int col = 12;
+        for (int i = 1; i <= col; i++) {
             int tt = rd.nextInt(3);
             map.get(tt).printBlock();
-            if(i==4||i==6||i==8)
+            if (i == 4 || i == 6 || i == 8)
                 System.out.println();
-            if(i==5||i==7)
+            if (i == 5 || i == 7)
                 System.out.print("    ");
         }
     }
+
     public static void main(String[] args) throws Exception {
         SimpleFactoryTest();
         System.out.println("");
@@ -89,12 +90,12 @@ public class App {
         Scanner scan = new Scanner(System.in);
         System.out.println("请输入指令（1输出普通地图，2输出魔法地图）");
         int order;
-        order  = scan.nextInt();
-        if(order == 1){
+        order = scan.nextInt();
+        if (order == 1) {
             AbstractFactoryMode(new NormalFactory());
-        }else{
+        } else {
             AbstractFactoryMode(new MagicFactory());
         }
         scan.close();
-    }  
+    }
 }
--- review/Design-Model/Creation mode/README.md
+++ review/Design-Model/Creation mode/README.md
@@ -3,5 +3,5 @@
 * [x] 单例模式(Singleton)
 * [x] 工厂模式(Factory)
 * [x] 抽象工厂模式(Abstract-Factory,合并到工厂模式目录)
-* [ ] 建造者模式(Builder)
-* [ ] 原型模式(Protype)
+* [x] 建造者模式(Builder)
+* [x] 原型模式(Protype)
